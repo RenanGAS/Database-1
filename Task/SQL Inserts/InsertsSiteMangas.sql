77fb59af422ddd191cbc96b6b07d7156547634dd
RenanGAS <renansakaoki@gmail.com>
2022-06-04 18:52:52 -0400

    fix: align some non-indented lines

--- Task/SQL Inserts/InsertsSiteMangas.sql
+++ Task/SQL Inserts/InsertsSiteMangas.sql
@@ -1,16 +1,16 @@
 INSERT INTO AVALIACAO(Like_, Dislike) VALUES(TRUE, FALSE), (FALSE, TRUE), (FALSE, FALSE);
 												
 INSERT INTO CATEGORIA(Nome, Imagem) VALUES('Ação', 'actionLogo.png'), ('Artes Marciais', 'martialArtsLogo.png'), 
-											  ('Aventura', 'adventureLogo.png'), ('Comédia', 'comedyLogo.png'), 
-                                              ('Drama', 'dramaLogo.png'), ('Escolar', 'schoolarLogo.png'),
-                                              ('Esportes', 'sportsLogo.png'), ('Fantasia', 'fantasyLogo.png'),
-                                              ('Magia', 'magicLogo.png'), ('Música', 'musicLogo.png');
+					  ('Aventura', 'adventureLogo.png'), ('Comédia', 'comedyLogo.png'), 
+                                          ('Drama', 'dramaLogo.png'), ('Escolar', 'schoolarLogo.png'),
+                                          ('Esportes', 'sportsLogo.png'), ('Fantasia', 'fantasyLogo.png'),
+                                          ('Magia', 'magicLogo.png'), ('Música', 'musicLogo.png');
 INSERT INTO ESTADO(Nome, Imagem) VALUES('Lendo', 'readingLogo.png'), ('Lerei', 'markedLogo.png'), 
-										   ('Lido', 'checkedLogo.png'), ('Parei', 'stoppedLogo.png'), 
-                                           ('Avaliei', 'classifiedLogo.png');
+				       ('Lido', 'checkedLogo.png'), ('Parei', 'stoppedLogo.png'), 
+                                       ('Avaliei', 'classifiedLogo.png');
 
 INSERT INTO NOTA(Numero) VALUES(0), (1), (2), (3), (4), (5), (6),
-							   (7), (8), (9), (10);
+		               (7), (8), (9), (10);
 
 # SELECT * FROM AUTOR;
 # SELECT * FROM AUTOR_TEM_MANGA;
