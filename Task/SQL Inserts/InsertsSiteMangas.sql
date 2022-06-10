b0ce1695ea593f793c524808efc01365f5eeaaf0
RENAN GUENSUKE AOKI SAKASHITA <renansakaoki@gmail.com>
2022-06-09 21:03:27 -0300

    feat: do more inserts in the Model's Database

--- Task/SQL Inserts/InsertsSiteMangas.sql
+++ Task/SQL Inserts/InsertsSiteMangas.sql
@@ -33,27 +33,37 @@ INSERT INTO MANGA(Nome, Imagem, ID_Situacao) VALUES('Naruto', 'narutoImg.png', 2
 						    ('Hunter x Hunter', 'hxhImg.png', 3), ('Haikyuu!!', 'hkImg.png', 2), 
                                                     ('One Punch Man', 'onepunchImg.png', 1), ('Kimetsu no Yaiba', 'knyImg.png', 1),
                                                     ('Shigatsu wa Kimi no Uso', 'shigatsuImg.png', 2), ('Vinland Saga', 'vinlandsagaImg.png', 1),
-                                                    ('Full Metal Alchemist', 'fullmetalImg.png', 2), ('Tokyo Revengers', 'tokyorevengersImg.png', 1);
+                                                    ('Full Metal Alchemist', 'fullmetalImg.png', 2), ('Tokyo Revengers', 'tokyorevengersImg.png', 1),
+                                                    ('Holy Avenger', 'hollyAImg.png', 2);
                                                     
 INSERT INTO AUTOR(Nome, Nacionalidade) VALUES('Masashi Kishimoto', 'Japonês'), ('Eiichiro Oda', 'Japonês'), 
 					('Yoshihiro Togashi', 'Japonês'), ('Haruichi Furudate', 'Japonês'),
                                         ('One', 'Japonês'), ('Koyoharu Gotouge', 'Japonês'),
                                         ('Naoshi Arakawa', 'Japonês'), ('Makoto Yukimura', 'Japonês'),
                                         ('Hiromu Arakawa', 'Japonês'), ('Ken Wakui', 'Japonês'),
-                                        ('Marcelo Cassaro', 'Brasileiro'), ('Caio Yo', 'Brasileiro');
+                                        ('Marcelo Cassaro', 'Brasileiro');
                                         
-INSERT INTO AUTOR_TEM_MANGA(ID_Autor, ID_Manga, NumeroMangas) VALUES(1, 11, 7), (2, 12, 6), (3, 13, 5), (4, 14, 4), (5, 15, 9), (6, 16, 3), (7, 17, 2),
-								    (8, 18, 2), (9, 19, 7), (10, 20, 0);
-                                                                    
+INSERT INTO AUTOR_TEM_MANGA(ID_Autor, ID_Manga, NumeroMangas) VALUES(1, 1, 7), (2, 2, 6), (3, 3, 5), (4, 4, 4), (5, 5, 9), (6, 6, 3), (7, 7, 2),
+								    (8, 8, 2), (9, 9, 7), (10, 10, 0), (11, 11, 2);
+                                    
+INSERT INTO CAPITULO(Titulo, NumeroPaginas, DataLancamento, ID_Manga) VALUES('Romance Dawn', 61, '2010-08-06', 2), ('O Homem "Luffy do Chapéu de Palha"', 25, '2010-08-06', 2), ('Surge Zoro, o Caçador de Piratas', 21, '2010-08-06', 2), ('O Capitão da Marinha: Morgan, Braço de Machado', 21, '2010-08-06', 2),
+('O Rei dos Piratas e o Melhor Espadachim', 21, '2010-08-06', 2), ('O Primeiro Membro', 23, '2010-08-06', 2), ('Amigos', 24, '2010-08-06', 2), ('Nami', 20, '2010-08-06', 2), ('Femme Fatale', 29, '2010-08-06', 2), ('Incidente na Taverna', 25, '2010-08-06', 2);
+
+INSERT INTO USUARIO_LE_CAPITULO(ID_Usuario, ID_Capitulo, ID_Avaliacao) VALUES(1, 1, 1), (2, 2, 1), (3, 3, 1), (4, 4, 1), (5, 5, 1), (6, 6, 1), (7, 7, 1), (8, 8, 1), (9, 9, 1), (10, 10, 1);
+
+INSERT INTO COMENTARIO(Mensagem, NumeroVotos, DataPublicacao, ID_Usuario_UsuarioLeCapitulo, ID_Capitulo_UsuarioLeCapitulo) VALUES('Já assisti o anime, agr quero ver algumas diferenças que ele tem com o mangá e é claro, ver acompanhar desde o início.', 12, '2022-04-09', 2, 2), ('Finalmente comecei essa obra !! Pretendo ler pelo menos até sabaody .. acho que depois disso vou começar o anime tambem', 8, '2022-04-09', 3, 3), ('já vi o anime mas quando cheguei no 930 e alguma coisa cansei, decidi ler o mangá agora e começar do zero', 2, '2022-04-28', 4, 4), ('Aqui estou eu, dando o primeiro passo na tão falada e elogiada obra One Piece.', 20, '2022-05-02', 5, 5), ('Acho que sou um pirata se pá... Mas serio, esses primeiros caps coloridos tão bonitões', 15, '2022-04-09', 6, 6), ('Cara eu tbm tô mto ansioso mas eu quero ler o manga inteiro até chegar nos atuais', 3, '2022-05-07', 7, 7), ('O Luffy tem uma intuição do baralho... interessante ver que o Luffy já confiava na sua força desde os primeiros capítulos, quando a pessoa lê assim no começo pensa que arrogância mais depois descobre outras coisas que faz parecer mais legal, e o Zoro muito top como sempre.', 22, '2022-03-21', 8, 8), ('Onigiri hm? Me lembra de um certo ataque', 5, '2022-06-03', 9, 9), ('meta é chegar no mil ainda esse fim de semana, mas os deveres num deixa kkkkk', 1, '2022-03-04', 10, 10), ('Zoro sola', 1000, '2022-06-09', 1, 1);   
+                                                                                                 
+INSERT INTO MANGA_PERTENCE_CATEGORIA(ID_Manga, ID_Categoria, NumeroMangas) VALUES(1, 1, 1), (1, 2, 1), (1, 3, 1), (2, 1, 2), (2, 3, 2), (2, 4, 1), (2, 8, 1), (3, 1, 3), (3, 3, 3), (3, 8, 2);                                                                                               
+
 # SELECT * FROM AUTOR; -------------------------
 # SELECT * FROM AUTOR_TEM_MANGA; ---------------
 # SELECT * FROM AVALIACAO; ---------------------
-# SELECT * FROM CAPITULO;
+# SELECT * FROM CAPITULO; ----------------------
 # SELECT * FROM CATEGORIA; ---------------------
-# SELECT * FROM COMENTARIO;
+# SELECT * FROM COMENTARIO; --------------------
 # SELECT * FROM ESTADO; ------------------------
 # SELECT * FROM MANGA; -------------------------
-# SELECT * FROM MANGA_PERTENCE_CATEGORIA;
+# SELECT * FROM MANGA_PERTENCE_CATEGORIA; ------
 # SELECT * FROM NOTA; --------------------------
 # SELECT * FROM REDESOCIAL; --------------------
 # SELECT * FROM SCANLATOR; ---------------------
@@ -63,4 +73,4 @@ INSERT INTO AUTOR_TEM_MANGA(ID_Autor, ID_Manga, NumeroMangas) VALUES(1, 11, 7), 
 # SELECT * FROM SITUACAO; ----------------------
 # SELECT * FROM USUARIO; -----------------------
 # SELECT * FROM USUARIO_ACESSA_MANGA;
-# SELECT * FROM USUARIO_LE_CAPITULO;
+# SELECT * FROM USUARIO_LE_CAPITULO; -----------
